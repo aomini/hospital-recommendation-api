de481f6e340f4358a1eed3ad850296952098e461
codavatarRakesh <r.shrestha@codavatar.tech>
2021-11-19 16:06:00 +0545

    field itms

---
+++ migrations/20211119100654-create-field-item.js
@@ -0,0 +1,49 @@
+"use strict";
+const migrationDefaultFields = require("../traits/database/migration-default-fields");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("field_items", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      field_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "fields",
+          },
+          key: "id",
+        },
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      subtitle: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      meta: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      ...migrationDefaultFields(Sequelize),
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("field_items");
+  },
+};
---
+++ models/FieldItem.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+const modelDefaultFields = require("../traits/database/model-default-fields");
+module.exports = (sequelize, DataTypes) => {
+  class FieldItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  FieldItem.init(
+    {
+      field_id: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      subtitle: DataTypes.STRING,
+      code: DataTypes.STRING,
+      type: DataTypes.STRING,
+      meta: DataTypes.JSONB,
+      ...modelDefaultFields(DataTypes),
+    },
+    {
+      sequelize,
+      paranoid: true,
+      underscored: true,
+      tableName: "fields",
+      modelName: "FieldItem",
+    }
+  );
+  return FieldItem;
+};
