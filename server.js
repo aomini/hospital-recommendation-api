7b956be2c9da69ab8044b9410c3e57127014bf3b
rakesh <developer@hospital.learning-club.me>
2021-11-19 07:39:31 +0000

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const morgan = require("morgan");
 const { sequelize } = require("./models");
@@ -15,6 +16,7 @@ const lookupValueRoutes = require("./routes/lookup-value");
 const bodyParser = require("body-parser");
 const port = process.env.PORT;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
