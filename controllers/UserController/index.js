5fffdf25527e1e01ef08d67f98c9fcfc20c00bec
codavatarRakesh <r.shrestha@codavatar.tech>
2021-11-23 14:12:52 +0545

    user update

--- controllers/UserController/index.js
+++ controllers/UserController/index.js
@@ -81,7 +81,8 @@ module.exports.updateUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    await user.update(req.body);
+    const { username, ...rest } = req.body;
+    await user.update(rest);
     res.status(200).json({
       message: "User Updated Successfully",
       success: true,
