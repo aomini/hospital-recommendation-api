3ac78ad8b928a80b7cffb27a63cc4146d6eacb1f
aashish rana magar <aashish.rana@ombryo.com>
2021-12-01 09:27:37 +0545

    userAuth removed

--- packages/server/routes/hospital.js
+++ packages/server/routes/hospital.js
@@ -12,7 +12,7 @@ const UserAuth = require("../middlewares/UserAuth");
 const router = express.Router();
 
 router.get("/", UserAuth, all);
-router.get("/basic", UserAuth, getHospitalsWithBasicData);
+router.get("/basic", getHospitalsWithBasicData);
 router.get("/:hospitalID", UserAuth, findHospital);
 router.post("/", UserAuth, create);
 router.put("/:hospitalID/:fieldID", UserAuth, update);
